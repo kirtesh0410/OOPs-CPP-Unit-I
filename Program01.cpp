07fa7f336eb3272ce3a81846771d6349f5ee7e48
Kirtesh Bharambe <kirteshbharambe0410@gmail.com>
2026-09-22 12:17:40 +0530

    Add files via upload

---
+++ Program01.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class SoilSensor {
+private:
+    string sensorId;
+    double moistureLevel;
+    string timestamp;
+
+public:
+    // Parameterized constructor
+    SoilSensor(string id, double moisture, string time)
+        : sensorId(id), moistureLevel(moisture), timestamp(time) {}
+
+    // Update sensor reading
+    void readSensor(double newMoisture, string newTime) {
+        moistureLevel = newMoisture;
+        timestamp = newTime;
+    }
+
+    // Display sensor data
+    void displayData() const {
+        cout << "Sensor: " << sensorId
+             << " | Moisture: " << moistureLevel << "%"
+             << " | Time: " << timestamp << endl;
+    }
+};
+
+int main() {
+    vector<SoilSensor> farmSensors;
+
+    // Create sensor objects
+    farmSensors.emplace_back("S001", 45.2, "08:00");
+    farmSensors.emplace_back("S002", 52.8, "08:00");
+    farmSensors.emplace_back("S003", 38.5, "08:00");
+
+    cout << "=== Morning Sensor Readings ===" << endl;
+
+    // Display all sensor readings
+    for (const auto& sensor : farmSensors) {
+        sensor.displayData();
+    }
+
+    // Update the first sensor
+    farmSensors[0].readSensor(47.5, "09:00");
+
+    cout << "\n=== Updated Reading ===" << endl;
+    farmSensors[0].displayData();
+
+    return 0;
+}
